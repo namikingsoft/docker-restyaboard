21b199e925647dd5d230b1fdf00907231d29b9d5
Tsubasa Namiki <namikingsoft@gmail.com>
2015-08-30 23:47:32 +0900

    Add docker spec

--- spec/restyaboard/service_spec.rb
+++ spec/restyaboard/service_spec.rb
@@ -8,3 +8,21 @@ end
 describe port(1234) do
   it { should be_listening }
 end
+
+describe docker_container('dockerrestyaboard_restyaboard_1') do
+  it { should be_running }
+end
+
+describe docker_container('dockerrestyaboard_postgres_1') do
+  it { should be_running }
+end
+
+describe docker_container('dockerrestyaboard_elasticsearch_1') do
+  it { should be_running }
+end
+
+describe docker_container('dockerrestyaboard_data00restyaboard_1') do
+  it { should have_volume('/usr/share/nginx/html/media','/volumes/data00restyaboard') }
+  it { should have_volume('/var/lib/postgresql/data','/volumes/data00postgres') }
+  it { should have_volume('/usr/share/elasticsearch/data','/volumes/data00elasticsearch') }
+end
