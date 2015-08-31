5864bdbaaea5bef336d77a14f6cf4ef54a127582
Tsubasa Namiki <namikingsoft@gmail.com>
2015-08-31 13:21:38 +0900

    Fixed degrade spec
    and modify README.md

--- spec/restyaboard/service_spec.rb
+++ spec/restyaboard/service_spec.rb
@@ -9,19 +9,19 @@ describe port(1234) do
   it { should be_listening }
 end
 
-describe docker_container('dockerrestyaboard_restyaboard_1') do
+describe docker_container('dockerfile_restyaboard_1') do
   it { should be_running }
 end
 
-describe docker_container('dockerrestyaboard_postgres_1') do
+describe docker_container('dockerfile_postgres_1') do
   it { should be_running }
 end
 
-describe docker_container('dockerrestyaboard_elasticsearch_1') do
+describe docker_container('dockerfile_elasticsearch_1') do
   it { should be_running }
 end
 
-describe docker_container('dockerrestyaboard_data00restyaboard_1') do
+describe docker_container('dockerfile_data00restyaboard_1') do
   it { should have_volume('/usr/share/nginx/html/media','/volumes/data00restyaboard') }
   it { should have_volume('/var/lib/postgresql/data','/volumes/data00postgres') }
   it { should have_volume('/usr/share/elasticsearch/data','/volumes/data00elasticsearch') }
